2c443a5ffe9141dcfc4e5c3afde16dc29cf1d977
Pragati Shendage <pragatishantilalshendage3677@gmail.com>
2026-07-27 19:19:17 +0530

    Implement JWT authentication and protected profile route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,10 +2,19 @@ const express = require("express");
 const router = express.Router();
 
 const authController = require("../controllers/authController");
+const protect = require("../middleware/authMiddleware");
 
-console.log(authController);
-
+// Public Routes
 router.post("/register", authController.registerUser);
 router.post("/login", authController.loginUser);
+
+// Protected Route
+router.get("/profile", protect, (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Protected route accessed successfully",
+    user: req.user,
+  });
+});
 
 module.exports = router;
